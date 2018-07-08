73ff273ef64f821b2f05cb0a6cf6be5e4820a77a
Tamara Kaufler <tkaufler@gmail.com>
2018-07-08 13:12:20 +0100

    Adds factorial unit test

--- fact-service/main_test.go
+++ fact-service/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func Test_fibonacci(t *testing.T) {
+func Test_factorial(t *testing.T) {
 	type args struct {
 		a uint64
 	}
@@ -14,7 +14,7 @@ func Test_fibonacci(t *testing.T) {
 		{
 			name: "zero",
 			args: args{0},
-			want: 0,
+			want: 1,
 		},
 		{
 			name: "one",
@@ -24,18 +24,18 @@ func Test_fibonacci(t *testing.T) {
 		{
 			name: "three",
 			args: args{3},
-			want: 2,
+			want: 6,
 		},
 		{
 			name: "ten",
-			args: args{10},
-			want: 55,
+			args: args{7},
+			want: 5040,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := fibonacci(tt.args.a); got != tt.want {
-				t.Errorf("fibonacci() = %v, want %v", got, tt.want)
+			if got := factorial(tt.args.a); got != tt.want {
+				t.Errorf("factorial() = %v, want %v", got, tt.want)
 			}
 		})
 	}
